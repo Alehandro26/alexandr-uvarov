ae334c9529c7852983b4b2e2334d8abd5c5ec89d
Alehandro26 <need26@yandex.ru>
2022-06-30 23:07:20 +0300

    Favicon

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import Home from './pages/Home'
 import Works from './pages/Works'
-import reportWebVitals from './reportWebVitals';
 import { HashRouter, Route, Routes } from 'react-router-dom';
 
 
@@ -23,7 +22,3 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
